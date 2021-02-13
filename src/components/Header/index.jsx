7e6aaf2919d64f048ab2b486d206c5c63aa097dd
leducloi <dekaito193@gmail.com>
2021-02-13 09:30:46 +0700

    style header

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import './Header.scss'
+function Header(props) {
+  return (
+    <div className="header container-fluid p-3">
+      <div className="row">
+        <h3 className="col-md-6 header__title">Soundcloud</h3>
+
+        <div className="col-md-6 header__search">
+          <input 
+            className="header__search__input" 
+            type="text" 
+            placeholder="Search here..."
+            autoFocus={true}
+          />
+          <i className="fas fa-search header__search__icon"></i>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+Header.propTypes = {
+
+}
+
+export default Header
+
