0e40a7df8354fffa532d1ccf9edcf16cca2f4a8f
leducloi <dekaito193@gmail.com>
2021-02-18 19:59:14 +0700

    add clear input value feature

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,14 +1,19 @@
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { useRef } from 'react';
 import debounce from '../../constants/optimize';
 import './Header.scss';
 function Header(props) {
+  const searchInputRef = useRef(null);
   const { onSearch } = props;
 
   const handleSearchWord = async (word) => {
     onSearch && onSearch(word);
   }
   const debounceSearchSong = debounce(handleSearchWord, 500);
+
+  const handleClearInput = () => {
+    searchInputRef.current.value = "";
+  }
 
   return (
     <div className="header container-fluid p-3">
@@ -17,7 +22,12 @@ function Header(props) {
           <h3 className="col-md-6 header__title">Soundcloud</h3>
 
           <div className="col-md-6 header__search">
+            <i 
+              className="fas fa-times-circle header__search--clear"
+              onClick={handleClearInput}
+            />
             <input 
+              ref={searchInputRef}
               className="header__search__input" 
               type="text" 
               placeholder="Search here..."
@@ -25,7 +35,7 @@ function Header(props) {
 
               onKeyUp={(e) => debounceSearchSong(e.target.value)}
             />
-            <i className="fas fa-search header__search__icon"></i>
+            <i className="fas fa-search header__search--search"/>
           </div>
         </div>
       </div>
